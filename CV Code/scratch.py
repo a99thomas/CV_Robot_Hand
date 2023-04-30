36c77ae6a253960433f629e006409d7c9f8343f6
a99thomas <117481301+a99thomas@users.noreply.github.com>
2023-04-30 12:02:46 -0600

    Update scratch.py

--- CV Code/scratch.py
+++ CV Code/scratch.py
@@ -16,18 +16,11 @@ resetExit = True
 
 
 #This code writes data to Serial Port
-# def Serialsend(y):
-#     y = y/2
-#     if y < 0:
-#         y = 0
-#     if y > 255:
-#         y = 255
-#     y = int(y)
-#     #print('sending', y)
-#     time.sleep(.00001)
-#     ardu.write(str(y).encode())
-#     time.sleep(.00001)
-#     #ardu.close()
+def Serialsend(anglenum, angleval):
+    #print('sending', y)
+    time.sleep(.0001)
+    message = str(anglenum) + ", " + str(angleval) + '\n'
+    ardu.write(message.encode())
 
 wrist = np.array([1.0,1.0,1.0])
 thumb_cmc = np.array([1.0,1.0,1.0])
@@ -54,21 +47,11 @@ pinky_tip = np.array([1.0,1.0,1.0])
 
 
 while True:
-   # ardu = serial.Serial('/dev/cu.usbmodem144301', 9600, timeout=.001)
+    ardu = serial.Serial('/dev/cu.usbserial-141140', 115200)  ##INSERT PORT NAME AND BAUD RATE
     success, image = cap.read()
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(imageRGB)
 
-    # checking whether a hand is detected
-    #if results.multi_hand_landmarks:
-
-     #   for hand_no, hand_landmarks in enumerate(results.multi_hand_landmarks):
-            #print(f'HAND NUMBER: {hand_no + 1}')
-            #print('---`--------------------')
-
-            #for i in range(21):
-               #print(f'{mpHands.HandLandmark(i).name}:')
-               #print(f'{hand_landmarks.landmark[mpHands.HandLandmark(i).value]}')
 
 
     if results.multi_hand_landmarks:
@@ -92,9 +75,7 @@ while True:
                     index_pip = np.array([lm.x, lm.y, lm.z])
                 if id == 7:
                     index_dip = np.array([lm.x, lm.y, lm.z])
-                if id == 8: #Index finger tip = ID 8
-                    # y = cy
-                    # Serialsend(y) #write landmark position to Serial
+                if id == 8:
                     index_tip = np.array([lm.x, lm.y, lm.z])
                     if (cx < 1300 and cx > 1158) and (cy > 0 and cy < 75):
                         escapeCount += 1
@@ -147,6 +128,22 @@ while True:
                     x1718 = pinky_mcp - pinky_pip
                     x1819 = pinky_pip - pinky_dip
                     x1920 = pinky_dip - pinky_tip
+
+                    x569 = (index_mcp+middle_mcp)/2 - index_pip
+                    x5910 = (index_mcp+middle_mcp)/2 - middle_pip
+
+                    x91013 = (middle_mcp+ring_mcp)/2 - middle_pip
+                    x91314 = (middle_mcp+ring_mcp)/2 - ring_pip
+
+                    x131417 = (ring_mcp+pinky_mcp)/2 - ring_pip
+                    x131718 = (ring_mcp + pinky_mcp)/2 - pinky_pip
+
+                    x08 = wrist - index_tip
+
+                    x59 = index_mcp - middle_mcp
+                    x913 = middle_mcp - ring_mcp
+                    x1317 = ring_mcp - pinky_mcp
+
                     angle1 = np.degrees(np.arccos(np.dot(x01,-x12)/(np.linalg.norm(x01) * np.linalg.norm(x12))))
                     angle2 = np.degrees(np.arccos(np.dot(x12,x23)/(np.linalg.norm(x12) * np.linalg.norm(x23))))
                     angle3 = np.degrees(np.arccos(np.dot(x23,-x34)/(np.linalg.norm(x23) * np.linalg.norm(x34))))
@@ -162,6 +159,11 @@ while True:
                     angle17 = np.degrees(np.arccos(np.dot(x017,-x1718)/(np.linalg.norm(x017) * np.linalg.norm(x1718))))
                     angle18 = np.degrees(np.arccos(np.dot(x1718,-x1819)/(np.linalg.norm(x1718) * np.linalg.norm(x1819))))
                     angle19 = np.degrees(np.arccos(np.dot(x1819,-x1920)/(np.linalg.norm(x1819) * np.linalg.norm(x1920))))
+                    angle20 = 2 * np.degrees(np.arccos(np.dot(x56, x910) / (np.linalg.norm(x56) * np.linalg.norm(x910))))
+                    angle21 = 2 * np.degrees(np.arccos(np.dot(x910,x1314)/(np.linalg.norm(x910) * np.linalg.norm(x1314))))
+                    angle22 = 2 * np.degrees(np.arccos(np.dot(x1314,x1718)/(np.linalg.norm(x1314) * np.linalg.norm(x1718))))
+                    angle23 = -angle20 / 2
+
                     if not np.isnan(angle1):
                         angle1 = int(angle1)
                     if not np.isnan(angle2):
@@ -169,45 +171,109 @@ while True:
                     if not np.isnan(angle3):
                         angle3 = int(angle3)
                     if not np.isnan(angle5):
-                        angle5 = int(angle5) + 50
+                        angle5 = int(angle5)
                     if not np.isnan(angle6):
                         angle6 = int(angle6)
                     if not np.isnan(angle7):
                         angle7 = int(angle7)
                     if not np.isnan(angle9):
-                        angle9 = int(angle9) + 40
+                        angle9 = int(angle9)
                     if not np.isnan(angle10):
                         angle10 = int(angle10)
                     if not np.isnan(angle11):
                         angle11 = int(angle11)
                     if not np.isnan(angle13):
-                        angle13 = int(angle13) + 40
+                        angle13 = int(angle13)
                     if not np.isnan(angle14):
                         angle14 = int(angle14)
                     if not np.isnan(angle15):
                         angle15 = int(angle15)
                     if not np.isnan(angle17):
-                        angle17 = int(angle17) + 38
+                        angle17 = int(angle17)
                     if not np.isnan(angle18):
                         angle18 = int(angle18)
                     if not np.isnan(angle19):
                         angle19 = int(angle19)
+                    if not np.isnan(angle20):
+                        angle20 = int(angle20)
+                    if not np.isnan(angle21):
+                         angle21 = int(angle21)
+                    if not np.isnan(angle22):
+                        angle22 = int(angle22)
+                    if not np.isnan(angle23):
+                        angle23 = int(angle23)
+                    # if 238-(angle13 + angle14 + angle15)/2 > 100:
+                    #     angle23 = 0
+
                     print("#############BREAK##########")
-                    print('Angle1: ', angle1)
-                    print('Angle2: ', angle2)
-                    print('Angle3: ', angle3)
-                    print('Angle5: ', angle5)
-                    print('Angle6: ', angle6)
-                    print('Angle7: ', angle7)
-                    print('Angle9: ', angle9)
-                    print('Angle10: ', angle10)
-                    print('Angle11: ', angle11)
-                    print('Angle13: ', angle13)
-                    print('Angle14: ', angle14)
-                    print('Angle15: ', angle15)
-                    print('Angle17: ', angle17)
-                    print('Angle18: ', angle18)
-                    print('Angle19: ', angle19)
+                    # print('Angle1: ', angle1) #THUMB CMC (In Palm)
+                    # print('Angle2: ', angle2) #Thumb Base Knuckle
+                    # print('Angle3: ', angle3) #Thumb Middle Knuckle
+
+                    ThumbKnuckles = 360 - angle2 - angle3
+                    ThumbKnuckles = Saturate(ThumbKnuckles, 0, 180)
+                    Serialsend(1, ThumbKnuckles)
+                    print('Thumb Knuckles', ThumbKnuckles)
+                    # print('Angle5: ', angle5) #Index Base Knuckle
+                    # print('Angle6: ', angle6) #Index Second Knuckle
+                    # print('Angle7: ', angle7) #Index Top Knuckle
+
+                    IndexKnuckles = 230-(angle5 + angle6 + angle7)/2
+                    IndexKnuckles = Saturate(IndexKnuckles, 0, 180)
+                    Serialsend(2, IndexKnuckles)
+                    print('Index Knuckles', IndexKnuckles)
+                    # print('Angle9: ', angle9) #Middle Base Knuckle
+                    # print('Angle10: ', angle10) #Middle Second Knuckle
+                    # print('Angle11: ', angle11) #Middle Top Knuckle
+
+                    MiddleKnuckles = 250-(angle9 + angle10 + angle11)/2
+                    MiddleKnuckles = Saturate(MiddleKnuckles, 0, 180)
+                    Serialsend(3, MiddleKnuckles)
+                    print('Middle Knuckles', MiddleKnuckles)
+                    # print('Angle13: ', angle13) #Ring Base Knuckle
+                    # print('Angle14: ', angle14) #Ring Second Knuckle
+                    # print('Angle15: ', angle15) #Ring Top Knuckle
+
+                    RingKnuckles = 238-(angle13 + angle14 + angle15)/2
+                    RingKnuckles = Saturate(RingKnuckles, 0, 180)
+                    Serialsend(4, RingKnuckles)
+                    print('Ring Knuckles', RingKnuckles)
+                    # print('Angle17: ', angle17) #Pinky Base Knuckle
+                    # print('Angle18: ', angle18) #Pinky Middle Knuckle
+                    # print('Angle19: ', angle19) #Pinky Top Knuckle
+
+                    PinkyKnuckles = 240-(angle17 + angle18 + angle19)/2
+                    PinkyKnuckles = Saturate(PinkyKnuckles, 0, 180)
+                    Serialsend(5, PinkyKnuckles)
+                    print('Pinky Knuckles', PinkyKnuckles)
+
+                    IndexTilt = abs(angle23)
+                    if IndexKnuckles > 90:
+                        IndexTilt = 0
+                    IndexTilt = Saturate(IndexTilt, 0, 25)
+                    Serialsend(7, IndexTilt)
+                    print('Ref ', angle23) #Ring Finger for reference
+
+                    MiddleTilt = angle23 + angle20
+                    if MiddleKnuckles > 85:
+                        MiddleTilt = 0
+                    MiddleTilt = Saturate(MiddleTilt, 0, 25)
+                    Serialsend(8, MiddleTilt)
+                    print('Angle20: ', MiddleTilt) #Between index and middle
+
+                    RingTilt = MiddleTilt+angle21
+                    if RingKnuckles > 100:
+                        RingTilt = MiddleTilt
+                    RingTilt = Saturate(RingTilt, 0, 35)
+                    Serialsend(9, RingTilt)
+                    print('Angle21: ', RingTilt) #Between Middle and Ring
+
+                    PinkyTilt = RingTilt + angle22/1.3 - 10
+                    if PinkyKnuckles > 100:
+                        PinkyTilt = RingTilt
+                    PinkyTilt = Saturate(PinkyTilt, RingTilt, 55)
+                    Serialsend(10, PinkyTilt)
+                    print('Angle22 ', PinkyTilt) #Between Ring and Pinky
             mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
 
 
@@ -218,5 +284,6 @@ while True:
         escapeCount = 0
 
     k = cv2.waitKey(33)
+   
     if k == 27 or escapeCount > 10:  # Esc key to stop
         break
